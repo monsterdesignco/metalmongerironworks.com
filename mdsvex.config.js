d1730809e6f53308b0ea8d52a9d7c5cc01614bb3
Hayden Koch <me@haydenkoch.com>
2023-10-18 17:51:06 -0700

    added layouts

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,5 +1,8 @@
 import { defineMDSveXConfig as defineConfig } from "mdsvex";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
+const dirname = path.resolve(fileURLToPath(import.meta.url), "../");
 const config = defineConfig({
   extensions: [".svelte.md", ".md", ".svx"],
 
@@ -9,6 +12,11 @@ const config = defineConfig({
 
   remarkPlugins: [],
   rehypePlugins: [],
+
+  layout: {
+    home: path.join(dirname, "./src/lib/layouts/home-layout.svelte"),
+    gallery: path.join(dirname, "./src/lib/layouts/gallery-layout.svelte"),
+  },
 });
 
 export default config;
---
+++ src/lib/layouts/home-layout.svelte
@@ -0,0 +1,5 @@
+<script>
+    export let title = "Change Me!"
+</script>
+
+{title}
