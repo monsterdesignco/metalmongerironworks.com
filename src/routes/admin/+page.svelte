8f7978708315ae8d5f0241608370b7042893979a
Hayden Koch <me@haydenkoch.com>
2023-10-18 17:32:45 -0700

    first commit

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { onMount } from "svelte";
+
+  onMount(async () => {
+    await import('https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js')
+    await import('https://identity.netlify.com/v1/netlify-identity-widget.js')
+  })
+
+</script>
