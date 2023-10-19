50680a6aec330b89b293af741c7d83425d8b4349
Hayden Koch <haydenkoch@NightmahersiMac.hsd1.ca.comcast.net>
2023-10-19 11:28:25 -0700

    7th commit

--- src/lib/components/content-section.svelte
+++ src/lib/components/content-section.svelte
@@ -3,20 +3,10 @@
     export let contentBackground = '';
 </script>
 
-<section class="bg-white dark:bg-gray-900" style="background-image: url('{contentBackground}')">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+<section class="bg-gray-900" style="background-image: url('{contentBackground}')">
+    <div class="py-20 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="text-center text-gray-900">
-            <h2 class="mb-4 text-4xl font-heading font-bold text-white lg:text-5xl dark:text-white">{contentTitle}</h2>
-            <a href="#"
-                class="inline-flex items-center text-lg font-medium text-primary-600 hover:text-primary-800 dark:text-primary-500 dark:hover:text-primary-700">
-                Learn more about inner source
-                <svg class="ml-1 w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                        clip-rule="evenodd"></path>
-                </svg>
-            </a>
+            <h2 class="mb-4 text-4xl font-heading font-bold text-white lg:text-5xl dark:text-white">{contentTitle}</h2>         
         </div>
         <div class="grid gap-6 mt-12 lg:mt-14 lg:gap-12 md:grid-cols-3">
             <div class="flex mb-2 md:flex-col md:mb-0">
