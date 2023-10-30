821bf15cb4bc4ed8f2bce3a3ecb1d73e17d8fb3d
Hayden Koch <me@haydenkoch.com>
2023-10-30 10:45:45 -0700

    added cloudinary integration

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,20 +1,21 @@
 <script>
+    import header from '$lib/content/header.json';
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte';
-    import Logo from '$lib/assets/images/nav-logo.png';
+
 </script>
   
   <Navbar class="bg-black py-6 !px-8 md:px-20">
     <NavBrand href="/" >
-      <img src={Logo} class="mr-3 h-16 lg:h-24" alt="Flowbite Logo" />
+      <img src={header.logo} class="mr-3 h-16 lg:h-24" alt="Flowbite Logo" />
     </NavBrand>
         <div class="flex md:order-2 pl-6">
-            <Button size="lg" class="uppercase font-semibold text-base">Get started</Button>
+            <Button href={header.buttonLink} size="lg" class="uppercase font-semibold text-base">{header.buttonText}</Button>
             <NavHamburger />
         </div>  
         <NavUl class="order-1 ml-auto uppercase">
-            <NavLi class="text-white font-semibold text-base" href="/" active={true}>Services</NavLi>
-            <NavLi class="text-white font-semibold text-base"href="/about">About</NavLi>
-            <NavLi class="text-white font-semibold text-base"href="/pricing">Gallery</NavLi>
-            <NavLi class="text-white font-semibold text-base" href="/contact">Contact</NavLi>
+            {#each header.navLinks as link}
+              <NavLi class="text-white font-semibold text-base" href={link.link}>{link.text}</NavLi>
+            {/each}
+
         </NavUl>
   </Navbar>
